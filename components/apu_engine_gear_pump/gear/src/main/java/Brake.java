a5861a35317259a651fd2513c40c0bbe18dbf6ee
Niel Rohling <niel.rohling@gmail.com>
2021-02-17 11:09:57 +0100

    Gear implementation

---
+++ components/apu_engine_gear_pump/gear/src/main/java/Brake.java
@@ -0,0 +1,27 @@
+public class Brake {
+    private String id;
+    private long dateOfManufacture;
+    int percentage;
+
+    public Brake(String id, long dateOfManufacture) {
+        this.id = id;
+        this.dateOfManufacture = dateOfManufacture;
+        this.percentage = 0;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public long getDateOfManufacture() {
+        return dateOfManufacture;
+    }
+
+    public int getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(int percentage) {
+        this.percentage = percentage;
+    }
+}
---
+++ components/apu_engine_gear_pump/gear/src/main/java/Gear.java
@@ -0,0 +1,110 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Gear {
+    // static instance
+    private static Gear instance = new Gear();
+    // port
+    public Port port;
+    private String manufacturer = "team 01";
+    private String id = "4775194 / 8864957";
+
+    private GearType type;
+    private ArrayList<Wheel> wheels = new ArrayList<>();
+    private boolean isDown = true;
+
+    // private constructor
+    private Gear() {
+        port = new Port();
+
+        wheels.addAll(List.of(
+                new Wheel("1", new Date().getTime(), new Brake("1", new Date().getTime())),
+                new Wheel("2", new Date().getTime(), new Brake("2", new Date().getTime()))
+        ));
+    }
+
+    // static method getInstance
+    public static Gear getInstance() {
+        return instance;
+    }
+
+    // inner methods
+    public String innerVersion() {
+        return "Gear // " + manufacturer + " - " + type + " - " + id;
+    }
+
+    public GearType innerSetGearType(String type) {
+        this.type = switch (type) {
+            case "front" -> GearType.front;
+            case "rear" -> GearType.rear;
+            default -> throw new IllegalStateException("Unexpected value: " + type);
+        };
+        return this.type;
+    }
+
+    public boolean innerDown() {
+        isDown = true;
+        return true;
+    }
+
+    public boolean innerUp() {
+        isDown = false;
+        return false;
+    }
+
+    public int innerSetBrake() {
+        return innerSetBrake(100);
+    }
+
+    public int innerSetBrake(int percentage) {
+        if (percentage > 100) {
+            percentage = 100;
+        }
+        if (percentage < 0) {
+            percentage = 0;
+        }
+        for (Wheel wheel : wheels) {
+            wheel.getBrake().setPercentage(percentage);
+        }
+        return percentage;
+    }
+
+    public int innerReleaseBrake() {
+        return innerSetBrake(0);
+    }
+
+
+    // inner class port
+    public class Port implements IGear {
+        public String version() {
+            return innerVersion();
+        }
+
+        public GearType setGearType(String type) {
+            return innerSetGearType(type);
+        }
+
+        public boolean down() {
+            return innerDown();
+        }
+
+        public boolean up() {
+            return innerUp();
+        }
+
+        public int setBrake() {
+            return innerSetBrake();
+        }
+
+        public int setBrake(int percentage) {
+            return innerSetBrake(percentage);
+        }
+
+        public int releaseBrake() {
+            return innerReleaseBrake();
+        }
+
+
+    }
+}
