12e5c7e5bcc608207e47e8bedc9ba879c404337d
Felix Käferlein <fel.kaeferlein.19@lehre.mosbach.dhbw.de>
2021-03-06 21:46:34 +0100

    removed ToDo

--- src/main/java/event/battery/BatteryDischarge.java
+++ src/main/java/event/battery/BatteryDischarge.java
@@ -2,7 +2,7 @@ package event.battery;
 
 public class BatteryDischarge {
 	private final int stateOfCharge = 80;
-	//TODO richtige Werte
+
 	public int getValue() { return stateOfCharge; }
 
 	@Override
